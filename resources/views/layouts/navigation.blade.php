1c19c603769b902468592954a0f133ed3e962337
AZARKANE Issam <issam.azarkane@gmail.com>
2024-07-05 14:48:03 +0200

    FIX container

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,59 +1,55 @@
-<nav class="navbar bg-base-100">
-    <div class="navbar-start">
-        <div class="dropdown">
-            <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
-                <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-5 w-5"
-                        fill="none"
-                        viewBox="0 0 24 24"
+<div class="bg-base-300 shadow-xl">
+    <nav class="navbar container mx-auto">
+        <div class="navbar-start">
+            <div class="dropdown">
+                <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
                         stroke="currentColor">
-                    <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M4 6h16M4 12h16M4 18h7" />
-                </svg>
-            </div>
-            <ul
-                    tabindex="0"
+                    </svg>
+                </div>
+                <ul tabindex="0"
                     class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                <li><a href="{{ route('dashboard') }}">Dashboard</a></li>
-                <li><a href="{{ route('programs.index') }}">Programmes</a></li>
-                <li><a href="{{ route('exercises.index') }}">Exercises</a></li>
-            </ul>
-        </div>
-    </div>
-    <div class="navbar-center">
-        <a href="{{ route('dashboard') }}" class="btn btn-ghost text-xl">Gymbro</a>
-    </div>
-    <div class="navbar-end">
-        @auth
-            <div class="dropdown dropdown-end">
-                <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-                    <div class="w-10 rounded-full">
-                        <img src="{{ asset("images/logo.svg") }}" alt="{{ Auth::user()->name }}" />
-                    </div>
-                </label>
-                <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                    <li>
-                        <a href="{{ route('profile.edit') }}" class="justify-between">
-                            Profile
-                        </a>
-                    </li>
-                    <li>
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-                            <a href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();">
-                                Logout
-                            </a>
-                        </form>
-                    </li>
+                    <li><a href="{{ route('dashboard') }}">Dashboard</a></li>
+                    <li><a href="{{ route('programs.index') }}">Programmes</a></li>
+                    <li><a href="{{ route('exercises.index') }}">Exercises</a></li>
                 </ul>
             </div>
-        @else
-            <a href="{{ route('login') }}" class="btn btn-ghost">Login</a>
-            <a href="{{ route('register') }}" class="btn btn-primary">Sign Up</a>
-        @endauth
-    </div>
-</nav>
+        </div>
+        <div class="navbar-center">
+            <a href="{{ route('dashboard') }}" class="btn btn-ghost text-xl">Gymbro</a>
+        </div>
+        <div class="navbar-end">
+            @auth
+                <div class="dropdown dropdown-end">
+                    <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+                        <div class="w-10 rounded-full">
+                            <img src="{{ asset('images/logo.svg') }}" alt="{{ Auth::user()->name }}" />
+                        </div>
+                    </label>
+                    <ul tabindex="0"
+                        class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li>
+                            <a href="{{ route('profile.edit') }}" class="justify-between">
+                                Profile
+                            </a>
+                        </li>
+                        <li>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <a href="{{ route('logout') }}"
+                                    onclick="event.preventDefault(); this.closest('form').submit();">
+                                    Logout
+                                </a>
+                            </form>
+                        </li>
+                    </ul>
+                </div>
+            @else
+                <a href="{{ route('login') }}" class="btn btn-ghost">Login</a>
+                <a href="{{ route('register') }}" class="btn btn-primary">Sign Up</a>
+            @endauth
+        </div>
+    </nav>
+</div>

3620989cf5b98f9a8007ad36d93f2c4ef9dc01fd
Tom PITORIN <tpitorin@myges.fr>
2024-07-05 14:35:07 +0200

    fix: now name and descrption are updated

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,7 @@ Route::middleware('auth')->group(function () {
 Route::middleware('auth')->group(function () {
     Route::get('/programs/edit/{id?}', [ProgramController::class, 'edit'])->name('programs.edit');
     Route::post('/programs', [ProgramController::class, 'store'])->name('programs.store');
+    Route::put('/programs/{program}', [ProgramController::class, 'update'])->name('programs.update');
     Route::put('/programs/{program}/exercises/{exerciseProgram}', [ProgramController::class, 'updateExercise'])->name('programs.updateExercise');
     Route::post('/programs/{program}/exercises', [ProgramController::class, 'addExercise']);
     Route::delete('/programs/{program}/exercises/{exerciseProgram}', [ProgramController::class, 'removeExercise'])->name('programs.removeExercise');

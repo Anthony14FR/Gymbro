62b25e3d97522a9f9f473966fa69aec1830dc724
AZARKANE Issam <issam.azarkane@gmail.com>
2024-07-21 22:16:55 +0200

    Edit page padding, navbar button

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,7 +1,7 @@
 <div class="bg-[#131417] shadow-md z-40 relative border-b-4 border-gray-500/20">
     <nav class="navbar flex justify-between container mx-auto">
         <div class="">
-            <div href="{{ route('subscriptions.index') }}" class="text-xl flex items-center gap-5">
+            <div href="{{ route('subscriptions.index') }}" class="text-xl flex items-center gap-5 p-3">
                 <img src="{{ asset('images/logo.svg') }}" alt="Gymbro" class="w-8 h-auto" />
                 <span class="text-xl font-bold text-white">GYMBRO</span>
                 @if (Auth::check())
@@ -26,9 +26,9 @@
         </div>
         <div class="">
             @auth
-                <div class="dropdown dropdown-end">
-                    <div tabindex="0" role="button" class="btn btn-accent m-1 text-white">Menu <i
-                            class="fa-solid fa-bars"></i></div>
+                <div class="dropdown dropdown-end lg:hidden block">
+                    <div tabindex="0" role="button" class="btn m-1 text-white">Menu <i class="fa-solid fa-bars"></i>
+                    </div>
                     <ul tabindex="0"
                         class="dropdown-content menu bg-base-200 text-white rounded-box z-[1] w-52 p-2 shadow">
                         <li><a href="{{ route('profile.edit') }}" class="">Profile</a></li>
@@ -41,13 +41,22 @@
                         </li>
                     </ul>
                 </div>
-            @else
-                <div class="space-x-3 bg-base-300/80 p-2">
-                    <a href="{{ route('login') }}" class="hover:underline">Login</a>
+                <div class="space-x-3 p-2 lg:flex hidden">
+                    <a href="{{ route('profile.edit') }}" class="hover:underline">Profile</a>
                     <span>|</span>
-                    <a href="{{ route('register') }}" class="hover:underline">Sign Up</a>
-                </div>
-            @endauth
-        </div>
+                    <a href="{{ route('programs.index') }}" class="hover:underline">Programs</a>
+                    <span>|</span>
+                    <form action="{{ route('logout') }}" method="post">
+                        @csrf
+                        <button type="submit" class="hover:underline">Logout</button>
+                    </form>
+                @else
+                    <div class="space-x-3 p-2">
+                        <a href="{{ route('login') }}" class="hover:underline">Login</a>
+                        <span>|</span>
+                        <a href="{{ route('register') }}" class="hover:underline">Sign Up</a>
+                    </div>
+                @endauth
+            </div>
     </nav>
 </div>
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,33 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
+
+    <div class="flex justify-between md:flex-row md:space-y-0 space-y-10 flex-col sm:px-6 lg:px-6 p-6">
+        <div class="breadcrumbs text-sm">
+            <span class="text-4xl font-normal">Profile <i class="fa-regular fa-address-card ml-2 text-accent"></i></span>
+            {!! Breadcrumbs::render() !!}
+        </div>
+    </div>
+
     <x-slot name="header">
         <span class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Profile') }}
         </span>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-base-200 border border-2 border-white/10 shadow sm:rounded-lg">
-                <div class="max-w-xl">
+    <div class="p-6">
+        <div class="grid md:grid-cols-2 grid-cols-1 gap-5">
+            <div class="p-4 sm:p-8 dark:bg-base-200 border border-2 border-white/10 shadow sm:rounded-lg">
+                <div class="">
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-base-200 border border-2 border-white/10 shadow sm:rounded-lg">
-                <div class="max-w-xl">
+            <div class="p-4 sm:p-8 dark:bg-base-200 border border-2 border-white/10 shadow sm:rounded-lg">
+                <div class="">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-base-200 border border-2 border-white/10 shadow sm:rounded-lg">
-                <div class="max-w-xl">
+            <div class="p-4 sm:p-8 dark:bg-base-200 border border-2 border-white/10 shadow sm:rounded-lg">
+                <div class="">
                     @include('profile.partials.delete-user-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-base-200 border border-2 border-white/10 shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 dark:bg-base-200 border border-2 border-white/10 shadow sm:rounded-lg">
                 <div class="max-w-xl space-y-5">
                     <span class="text-2xl font-medium text-gray-900 dark:text-gray-100">
                         {{ __('Subscription Management') }}
@@ -50,13 +58,16 @@
                             <p>You are subscribed to the <span class="font-semibold text-yellow-600">Premium</span> plan.</p>
                             <form action="{{ route('subscriptions.unsubscribe') }}" method="POST">
                                 @csrf
-                                <button type="submit" class="btn btn-danger mt-2">Unsubscribe</button>
+                                <button type="submit" class="btn btn-neutral mt-2">Unsubscribe</button>
                             </form>
                         @else
                             <p>You do not have an active subscription.</p>
+                            <a href="{{ route('subscriptions.index') }}" class="btn btn-accent mt-2">Subscribe</a>
                         @endif
                     @else
                         <p>You do not have an active subscription.</p>
+                        <a href="{{ route('subscriptions.index') }}" class="btn btn-accent mt-2">Subscribe</a>
+                        
                     @endrole
                 </div>
             </div>

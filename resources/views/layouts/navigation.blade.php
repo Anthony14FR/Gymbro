e80915eba97ce7bbc6b9c48a9a68ae08af04c9a9
Tom PITORIN <tpitorin@myges.fr>
2024-07-25 02:44:18 +0200

    removing view plans in landing page + adding programs redirection on navbar logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,22 +2,24 @@
     <nav class="navbar flex justify-between container mx-auto">
         <div class="">
             <div href="{{ route('subscriptions.index') }}" class="text-xl flex items-center gap-5">
-               <img src="{{ asset('images/logo.svg') }}" alt="Gymbro" class="w-8 h-auto" />
-                <span class="ml-4 text-xl font-bold text-white">GYMBRO</span>
+                <a href="{{ route('programs.index') }}" class="text-xl flex items-center gap-5">
+                    <img src="{{ asset('images/logo.svg') }}" alt="Gymbro" class="w-8 h-auto"/>
+                    <span class="ml-4 text-xl font-bold text-white">GYMBRO</span>
+                </a>
                 @if (Auth::check())
                     @if (Auth::check() && Auth::user()->hasRole('admin'))
                         <span class="text-xs bg-yellow-300 font-bold text-black/70 px-2 py-1 rounded-full">Admin</span>
                         <a href="{{ route('users.index') }}" class="text-xs btn">Dashboard</a>
                     @elseif (Auth::check() && Auth::user()->hasRole('premium'))
                         <span
-                            class="text-xs bg-yellow-300 font-bold text-black/70 px-2 py-1 rounded-full">Premium</span>
+                                class="text-xs bg-yellow-300 font-bold text-black/70 px-2 py-1 rounded-full">Premium</span>
                     @elseif (Auth::check() && Auth::user()->hasRole('user'))
                         <a href="{{ route('subscriptions.index') }}" class="text-xl flex items-center gap-5">
                             <div
-                                class="bg-yellow-300 font-bold text-black/70 flex px-2 py-1 rounded-full ml-6 cursor-default">
+                                    class="bg-yellow-300 font-bold text-black/70 flex px-2 py-1 rounded-full ml-6 cursor-default">
                                 <span class="text-xs cursor-default">Pas encore abonné ? Clique <a
-                                        class="underline ml-1 text-xs cursor-pointer"
-                                        href="{{ route('subscriptions.index') }}">ici<a></span>
+                                            class="underline ml-1 text-xs cursor-pointer"
+                                            href="{{ route('subscriptions.index') }}">ici<a></span>
                             </div>
                         </a>
                     @endif
@@ -28,7 +30,7 @@
             @auth
                 <div class="dropdown dropdown-end">
                     <div tabindex="0" role="button" class="btn btn-accent m-1 text-white">Menu <i
-                            class="fa-solid fa-bars"></i></div>
+                                class="fa-solid fa-bars"></i></div>
                     <ul tabindex="0"
                         class="dropdown-content menu bg-base-200 text-white rounded-box z-[1] w-52 p-2 shadow">
                         <li><a href="{{ route('profile.edit') }}" class="">Profile</a></li>

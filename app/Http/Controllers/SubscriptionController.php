0032c61f181eeb0332cbe726d97354bbd624d712
Tom PITORIN <tpitorin@myges.fr>
2024-07-25 03:34:36 +0200

    fix date subscriptions

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -68,7 +68,7 @@ class SubscriptionController extends Controller
                 'stripe_id' => $session->customer,
                 'stripe_subscription_id' => $session->subscription,
                 'stripe_plan' => $plan_id,
-                'ends_at' => Carbon::createFromTimestamp($session->expires_at),
+                'ends_at' => Carbon::createFromTimestamp($session->expires_at)->addMonth(),
                 'isCancelled' => false,
             ]);
             $user->assignRole('premium');
